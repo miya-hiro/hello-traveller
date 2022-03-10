f257d5d7155dcdef9bcd0316a48eaa74587d06b8
miya-hiro <52573499+miya-hiro@users.noreply.github.com>
2022-03-10 21:58:21 +0900

    weather,tweetでそれぞれget送信, メソッド切り出し

--- resources/js/weather.js
+++ resources/js/weather.js
@@ -5,12 +5,11 @@ $(function () {
     var destination = $(this).val();
     // console.log('changeイベント');
     // console.log(destination);
-
     $.ajax({
       headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
       },
-      url: '/get-ajax',
+      url: '/api/weather',
       type: 'GET',
       data: {
         'destination': destination
@@ -21,75 +20,63 @@ $(function () {
         // console.log("success");
         console.log(response);
 
-        //
-        //ここから天気処理
-        //
         var $weatherWrap = $('#js_weatherWrap')
         $weatherWrap.html(""); //前回の取得内容をリセット
         // console.log('天気ここから');
         // console.log($weatherWrap);
 
         var block = '<div class="col">'
-          + '<p>今のお天気：' + response.weather.weather + '</p>'
-          + '<p><img src="https://openweathermap.org/img/wn/' + response.weather.icon + '@2x.png"></p>'
+          + '<p>今のお天気：' + response.data + '</p>'
+          + '<p><img src="https://openweathermap.org/img/wn/' + response.icon + '@2x.png"></p>'
           + '</div>';
         // console.log(block);
         $weatherWrap.append(block);
 
+      })
+      .fail(function () {
+        console.log("failed");
+      })
 
-        //
-        //ここからtweet処理
-        //
+
+    $.ajax({
+      headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+      },
+      url: '/api/tweets',
+      type: 'GET',
+      data: {
+        'destination': destination
+      },
+      dataType: 'json',
+    })
+      .done(function (response) {
+        // console.log("success");
+
+        const makeTweetHtml = function (index, tweet) {
+
+          var block = '<div class="col">'
+            + '<a href="https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str + '" class="test-dark" target="_blank">'
+            + '<p>画像：<img src="' + tweet.mediaUrl + '" class="img-fluid"></p>'
+            + '<p>ツイート内容：' + tweet.full_text + '</p></a>'
+            + '</div>';
+
+          targetDom.append(block);
+        }
 
         //
         //天気
         //
-        var $tweetWeatherWrap = $('#js_tweetWeatherWrap')
-        // console.log($tweetWrap.html());
-
-        $tweetWeatherWrap.html(""); //前回の取得内容をリセット
-
-        $.each(response.tweetWeatherList, function (index, tweet) {
-
-          // console.log(tweet);
-          // console.log(tweet.mediaUrl);
-
-          var block = '<div class="col">'
-            + '<a href="https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str + '" class="test-dark" target="_blank">'
-            // + '<p>アイコン：<img src="' + tweet.user.profile_image_url_https + '"></p>'
-            + '<p>画像：<img src="' + tweet.mediaUrl + '" class="img-fluid"></p>'
-            + '<p>ツイート内容：' + tweet.full_text + '</p></a>'
-            + '</div>';
-
-          // console.log(block);
-
-          $tweetWeatherWrap.append(block);
-
-
-        }) //each１つめここまで
+        var targetDom = $('#js_tweetWeatherWrap')
+        targetDom.html(""); //前回の取得内容をリセット
+        $.each(response.weather, makeTweetHtml) //each１つめここまで
 
         //
         //食べ物
         //
-        var $tweetFoodWrap = $('#js_tweetFoodWrap')
-        // console.log($tweetWrap.html());
+        var targetDom = $('#js_tweetFoodWrap')
+        targetDom.html(""); //前回の取得内容をリセット
+        $.each(response.food, makeTweetHtml) //each１つめここまで
 
-        $tweetFoodWrap.html(""); //前回の取得内容をリセット
-
-        $.each(response.tweetFoodList, function (index, tweet) {
-
-          var block = '<div class="col">'
-            + '<a href="https://twitter.com/' + tweet.user.screen_name + '/status/' + tweet.id_str + '" class="test-dark" target="_blank">'
-            // + '<p>アイコン：<img src="' + tweet.user.profile_image_url_https + '"></p>'
-            + '<p>画像：<img src="' + tweet.mediaUrl + '" class="img-fluid"></p>'
-            + '<p>ツイート内容：' + tweet.full_text + '</p></a>'
-            + '</div>';
-
-          // console.log(block);
-
-          $tweetFoodWrap.append(block);
-
-        }) //each２つめここまで
       })
       .fail(function () {
         console.log("failed");
