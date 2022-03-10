0fc9065988501993938bd39a38015977afd0ed94
miya-hiro <52573499+miya-hiro@users.noreply.github.com>
2022-03-10 21:57:26 +0900

    web,api, 欲しい物別でファイル分け

---
+++ app/Http/Controllers/api/TwitterController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Abraham\TwitterOAuth\TwitterOAuth;
+use Illuminate\Http\Request;
+
+class TwitterController extends Controller
+{
+    private $connection;
+
+    public function __construct()
+    {
+        $this->connection = new TwitterOAuth(
+            config('const.api.twitter.key'),
+            config('const.api.twitter.key_secret'),
+            config('const.api.twitter.access_token'),
+            config('const.api.twitter.access_token_secret')
+        );
+    }
+
+    public function getTweets(Request $request)
+    {
+        $destination = $request->destination;
+
+        //指定ワード
+        // $q =  $destination . ' 天気 -相互 filter:images -#相互RT exclude:retweets -#OpenWeatherMap -from:mint_tanpopo';
+
+        $tweetWeatherList = $this->getTweetByKeywords($destination, '#イマソラ');
+
+        $tweetFoodList = $this->getTweetByKeywords($destination, '美味しい');
+
+        return response()->json(
+            [
+                'weather' => $tweetWeatherList,
+                'food' => $tweetFoodList,
+            ]
+        );
+    }
+
+    private function getTweetByKeywords($destination, $keyword)
+    {
+        $q =  $destination . $keyword . ' -相互 filter:images -#相互RT exclude:retweets';
+        // dd($q);
+
+        $tweets_params = [
+            'q' => $q,
+            'count' => config('const.api.twitter.settings.getNum'),
+            'tweet_mode' => 'extended', //ここ！(text->full_text) 画像表示が直る
+        ];
+        // dd($tweets_params);
+        $tweetList = $this->connection->get('/search/tweets', $tweets_params)->statuses;
+        // dd($tweetList);
+        foreach ($tweetList as $tweet) {
+            $tweet->mediaUrl = '画像なし'; //初期値
+
+            //画像urlを取得
+            $mediaExist = $tweet->extended_entities->media ?? '';
+
+            if ($mediaExist) {
+                $media = $tweet->extended_entities->media;
+                foreach ($media as $m) {
+                    if ($m->type === 'photo') {
+                        $tweet->mediaUrl = $m->media_url_https;
+                    }
+                }
+            }
+        }
+
+        return $tweetList;
+    }
+}
---
+++ app/Http/Controllers/api/WeatherController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class WeatherController extends Controller
+{
+    public function getWeather(Request $request)
+    {
+        $destination = $request->destination;
+
+        $lat = config('const.positions.' . $destination . '.lat');
+        $lon = config('const.positions.' . $destination . '.lon');
+
+        $appid = config('const.api.weather.key');
+
+        $url = "http://api.openweathermap.org/data/2.5/weather/?lat={$lat}&lon={$lon}&appid={$appid}";
+
+        //天気取得に必要なデータ取得URL
+        // $url = "http://api.openweathermap.org/geo/1.0/direct?q=Sapporo&limit=5&appid={$appid}";
+
+        $weather_json = file_get_contents($url);
+        $weather_array = json_decode($weather_json, true);
+
+        // $weather = [];
+        // $weather['weather'] = $weather_array['weather'][0]['main'];
+        // $weather['icon'] = $weather_array['weather'][0]['icon'];
+
+        return response()->json(
+            [
+                'data' => $weather_array['weather'][0]['main'],
+                'icon' => $weather_array['weather'][0]['icon'],
+            ]
+        );
+    }
+}
