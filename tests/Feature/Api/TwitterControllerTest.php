28dd4675d5e0f94b71b6d859cbb95c7cf4cc2d47
miya-hiro <52573499+miya-hiro@users.noreply.github.com>
2022-04-03 14:06:41 +0900

    *

---
+++ tests/Feature/Api/TwitterControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TwitterControllerTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @test
+     * @covers \App\Controllers\Api\TwitterController::getTweets
+     */
+    public function getTweets()
+    {
+        $data = [
+            'destination' => '東京'
+        ];
+
+        $response = $this->get('api/tweets', $data);
+dd($response);
+        $response->assertStatus(200);
+    }
+}
