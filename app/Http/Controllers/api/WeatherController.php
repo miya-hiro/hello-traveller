61c720aeafae70a352b91010b87b02747583a6f4
miya-hiro <52573499+miya-hiro@users.noreply.github.com>
2022-04-06 09:36:17 +0900

    *

--- app/Http/Controllers/api/WeatherController.php
+++ app/Http/Controllers/api/WeatherController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Abraham\TwitterOAuth\TwitterOAuth;
 
 use Illuminate\Http\Request;
 
@@ -20,9 +19,6 @@ class WeatherController extends Controller
 
         $url = "http://api.openweathermap.org/data/2.5/weather/?lat={$lat}&lon={$lon}&appid={$appid}";
 
-        //天気取得に必要なデータ取得URL
-        // $url = "http://api.openweathermap.org/geo/1.0/direct?q=Sapporo&limit=5&appid={$appid}";
-
         $weather_json = file_get_contents($url);
         $weather_array = json_decode($weather_json, true);
 
@@ -32,5 +28,8 @@ class WeatherController extends Controller
                 'icon' => $weather_array['weather'][0]['icon'],
             ]
         );
+
+         //天気取得に必要なデータ取得URL
+        // $url = "http://api.openweathermap.org/geo/1.0/direct?q=Sapporo&limit=5&appid={$appid}";
     }
 }
